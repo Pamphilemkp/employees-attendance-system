64ea9757217c0485444aa386370595365fc22039
pamphilemkp <pamphilemkp@gmail.com>
2024-09-30 15:05:45 +0000

    fix build errros with babel

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,17 @@
 /* eslint-disable */
 'use client';
 
-import { Inter } from 'next/font/google';
 import { SessionProvider } from 'next-auth/react';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <head>
+        {/* Link to Google Fonts */}
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="font-inter">
         <SessionProvider>
           {children}
         </SessionProvider>
